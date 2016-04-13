42c61f9c64f4cea7e06ad2faef0e7cc95438dd5b
António Simões <asimoes@student.dei.uc.pt>
2016-04-13 01:52:01 +0100

    Filter Menu added

--- TP3/Ex3.m
+++ TP3/Ex3.m
@@ -8,16 +8,28 @@
 F = fftshift(fft2(double(img)));
 % mesh(20*log10(abs(F)));
 
-%%% Fazer menu
-fc = 50;
-
 [X,Y] = meshgrid(1:linhas, 1:colunas);
-mask((linhas/2)-fc:(linhas/2)+fc, (colunas/2)-fc:(colunas/2)+fc) = 1;
-mask = double(mask);
-mesh(mask);
+
+fc = input('Frequencia de corte = ');
+option = menu('Escolher Filtro','Passa-Baixo','Passa-Alto');
+if option == 1
+    mask = zeros(linhas);
+    mask((linhas/2)-fc:(linhas/2)+fc, (colunas/2)-fc:(colunas/2)+fc) = 1;
+    mask = double(mask);
+    mesh(mask);
+    title('Magnitude Mascara Passa-Baixo');
+else
+    mask = ones(linhas);
+    mask((linhas/2)-fc:(linhas/2)+fc, (colunas/2)-fc:(colunas/2)+fc) = 0;
+    mask = double(mask);
+    mesh(mask);
+    title('Magnitude Mascara Passa-Alto');
+end
+rotate3d on;
 
 C = mask.*F;
 D = ifft2(C);
 D = uint8(D);
 figure
 imshow(D);
+rotate3d on;
