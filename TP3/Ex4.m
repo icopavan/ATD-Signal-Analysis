deafdd1393523fffd68572502f47f3ed707e2dee
António Simões <asimoes@student.dei.uc.pt>
2016-04-28 00:25:32 +0100

    Som a reproduzir. Magnitude do sinal plot

---
+++ TP3/Ex4.m
@@ -0,0 +1,30 @@
+%Exercicio 4
+
+close all;
+clear all;
+clc
+
+%4.1
+disp('4.1');
+file = 'saxriff.wav';
+
+info = audioinfo(file);
+nbits = info.BitsPerSample;
+ 
+[y, fs] = audioread(file);
+sound(y, fs);
+
+pause();
+
+ws= 2 * pi * fs;
+
+size_y = length(y);
+x = fftshift(fft(y));
+
+if mod(size_y, 2) == 0
+    f = -ws/2 : ws/size_y : (ws/2) - (ws/size_y);
+else
+    f = -ws/2 + ((ws/2)/size_y): ws/size_y: (ws/2) - ((ws/2)/size_y);
+end
+
+figure(1), plot(f, abs(x)), title('Magnitude do espectro do sinal');  
