95a5b4c3f790f42b93cf17882a0cd4e9f88fa380
António Simões <asimoes@student.dei.uc.pt>
2016-04-28 00:12:51 +0100

    Graficos da mascara acrescentados. Tudo a funcionar

--- TP3/Ex3.m
+++ TP3/Ex3.m
@@ -1,17 +1,34 @@
 %%% EX 3
 
+close all;
+clc;
+
 % 3.1
 [img, color_map, transparency] = imread('Peppers.bmp');
-[linhas, colunas] = size(img);
+figure(1);
+imshow(img, color_map);
+title('Representacao de Peppers.bmp');
+
+[linhas, colunas] = size(img); % 512 x 512
 
 % 3.3
 F = fftshift(fft2(double(img)));
-% mesh(20*log10(abs(F)));
+
+xx = -linhas/2:linhas/2-1;
+yy = -colunas/2:colunas/2-1;
+
+figure(2);
+mesh(xx, yy, 20*log10(abs(F)));
+axis([xx(1) xx(end) yy(1) yy(end)]);
+view([-37.5 30]);
+rotate3d;
+title('Magnitude da Imagem');
 
 [X,Y] = meshgrid(1:linhas, 1:colunas);
 
-fc = input('Frequencia de corte = ');
 option = menu('Escolher Filtro','Passa-Baixo','Passa-Alto');
+fc = input('Frequencia de corte = ');
+figure(3);
 if option == 1
     mask = zeros(linhas);
     mask((linhas/2)-fc:(linhas/2)+fc, (colunas/2)-fc:(colunas/2)+fc) = 1;
@@ -27,9 +44,15 @@ else
 end
 rotate3d on;
 
-C = mask.*F;
-D = ifft2(C);
-D = uint8(D);
-figure
+filtro = mask.*F;
+D = uint8(ifft2(filtro));
+
+figure(4);
+mesh(xx, yy, 20*log10(abs(filtro)));
+rotate3d;
+title('Magnitude do Filtro');
+
+figure(5)
 imshow(D);
 rotate3d on;
+title('Peppers.bmp com Filtro aplicado');
